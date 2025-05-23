7302a4ba78f63309d70583e1651bed030a2018f7
Avtushko <a.avtushko86@gmail.com>
2025-05-23 17:10:49 +0300

    lesson_01

---
+++ lesson_01/lesson.py
@@ -0,0 +1,16 @@
+def sub(a, b):
+    return a-b
+
+
+x = sub(5, 7)
+
+
+print(x)
+
+
+def div(c, d):
+    return c / d
+
+
+x = div(15, 11)
+print(x)
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,10 @@
+def print_num(num):
+    print(num, end='')
+
+
+digits = [8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5]
+for digit in digits:
+    print_num(digit)
+
+
+print()
